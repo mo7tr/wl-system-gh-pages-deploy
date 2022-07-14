7e94f6e26aa71c1bd82960aff78d6699178c8781
mo7tr <mo7trading@gmail.com>
2022-07-14 17:37:38 +0200

    Updates

---
+++ precache-manifest.9837e4de659b227798c69ccebf82c1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db10c9849e9e7489496f96c14b217c65",
+    "url": "/wl-system-gh-pages-deploy/index.html"
+  },
+  {
+    "revision": "c36468dd3defd89dc864",
+    "url": "/wl-system-gh-pages-deploy/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "7b3c16d833dfc2f338c6",
+    "url": "/wl-system-gh-pages-deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c36468dd3defd89dc864",
+    "url": "/wl-system-gh-pages-deploy/static/js/2.a29e1cda.chunk.js"
+  },
+  {
+    "revision": "7b3c16d833dfc2f338c6",
+    "url": "/wl-system-gh-pages-deploy/static/js/main.b364ada9.chunk.js"
+  },
+  {
+    "revision": "e05680fc8bf2e60be2c2",
+    "url": "/wl-system-gh-pages-deploy/static/js/runtime-main.9c8243ae.js"
+  }
+]);
